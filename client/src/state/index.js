a422dccd4558a35d5a6cafdc1f1e5c91ecc4a114
danypepin1 <dany.pepin.1@ulaval.ca>
2023-06-06 22:43:24 -0400

    add login/register

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -43,5 +43,5 @@ export const authSlice = createSlice({
 
     }
 })
-export const { setMode, setLogout, setFriends, setPosts,setPost } = authSlice.actions;
+export const { setMode, setLogin, setLogout, setFriends, setPosts,setPost } = authSlice.actions;
 export default authSlice.reducer;
